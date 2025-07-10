f78335625f8de8677c0faad4eb50756d5181dfa8
nigelwrightington <nigelwrightington@yahoo.com>
2025-07-09 23:11:19 -0400

    Submitted full assignment

---
+++ index.js
@@ -0,0 +1,19 @@
+let myVariable = "code1";
+
+let code1 = 7 + 3;
+
+let code2 = (81-1) / 2;
+
+let code3 = (10 % 5) + 39;
+
+let message = "The vault has been secured. The combination is:";
+
+let codeA = "" + code1 + "-" + code2 + "-" + code3;
+
+let codeB = `${code1}-${code2}-${code3}`;
+
+console.log(message);
+
+console.log(codeA);
+
+console.log(codeB);
